bc4ecbfe49c5948aab040afd2dade85eefaf8846
Twayfrancis <tawaifrancis@gmail.com>
2023-05-24 12:57:23 -0700

    tests

--- mty_palpush.c
+++ mty_palpush.c
@@ -1,33 +1,38 @@
 #include "monty.h"
-#include <stdio.h>
-#include <stdlib.h>
-
+/**
+ * pall - prints all the elements in the stack
+ * @stack: double pointer to the top of the stack
+ */
 void pall(stack_t **stack)
 {
-    stack_t *current = *stack;
+	stack_t *current = *stack;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
-
+/**
+ * push - pushes an element to the top of the stack
+ * @stack: double pointer to the top of the stack
+ * @value: value to be pushed onto the stack
+ */
 void push(stack_t **stack, int value)
 {
-    stack_t *new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Memory allocation failed\n");
-        exit(EXIT_FAILURE);
-    }
+	stack_t *new_node = malloc(sizeof(stack_t));
 
-    new_node->n = value;
-    new_node->prev = NULL;
-    new_node->next = *stack;
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Memory allocation failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    if (*stack != NULL)
-        (*stack)->prev = new_node;
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
 
-    *stack = new_node;
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+	*stack = new_node;
 }
