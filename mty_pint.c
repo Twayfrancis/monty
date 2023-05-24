057f7ac8c2d79f21a5e79dad8057bec493ce1ffd
Twayfrancis <tawaifrancis@gmail.com>
2023-05-24 13:15:20 -0700

    fix

--- mty_pint.c
+++ mty_pint.c
@@ -1,12 +1,15 @@
 #include "monty.h"
-
+/**
+ * pint - prints value at top of stack
+ * @stack: double pointer to head of the stack
+ * @line_number: the line number of the instruction being executed
+ */
 void pint(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL)
-    {
-        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("%d\n", (*stack)->n);
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
 }
--- mty_pop.c
+++ mty_pop.c
@@ -1,20 +1,21 @@
 #include "monty.h"
-
+/**
+ * pop - removes top element of stack
+ * @stack: double pointer to head stack
+ * @line_number: line num of instruction being executed
+ */
 void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp = *stack;
 
-    if (*stack == NULL)
-    {
-        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	*stack = (*stack)->next;
 
-    /*stack_t *temp = *stack;*/
-    *stack = (*stack)->next;
-
-    if (*stack != NULL)
-        (*stack)->prev = NULL;
-
-    free(temp);
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(temp);
 }
